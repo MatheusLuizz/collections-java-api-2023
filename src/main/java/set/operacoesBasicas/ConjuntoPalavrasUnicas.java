6dcca800e91b552d4bec8fb5d284994d1f11ce00
Matheus Luiz <matheus.luizf35@gmail.com>
2023-08-19 23:54:29 -0300

    Realizando atividades de Set: Conjunto de Convidados e Conjunto de Palavras Únicas
    Signed-off-by: Matheus Luiz <matheus.luizf35@gmail.com>

---
+++ src/main/java/set/operacoesBasicas/ConjuntoPalavrasUnicas.java
@@ -0,0 +1,45 @@
+package main.java.set.operacoesBasicas;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConjuntoPalavrasUnicas {
+    private Set<String> palavrasUnicas;
+
+    public ConjuntoPalavrasUnicas() {
+        this.palavrasUnicas = new HashSet<>();
+    }
+    public void adicionarPalavra(String palavra) {
+        palavrasUnicas.add(palavra);
+    }
+    public void removerPalavra(String palavra) {
+        if (!palavrasUnicas.isEmpty()) {
+            palavrasUnicas.remove(palavra);
+        }
+    }
+    public boolean verificarPalavra (String palavra) {
+        return palavrasUnicas.contains(palavra);
+    }
+    public void exibirPalavrasUnicas() {
+        System.out.println(palavrasUnicas);
+    }
+
+    public static void main(String[] args) {
+        ConjuntoPalavrasUnicas conjuntoPalavrasUnicas = new ConjuntoPalavrasUnicas();
+
+        conjuntoPalavrasUnicas.adicionarPalavra("Palavra 1");
+        conjuntoPalavrasUnicas.adicionarPalavra("Palavra 1");
+        conjuntoPalavrasUnicas.adicionarPalavra("Palavra 3");
+        conjuntoPalavrasUnicas.adicionarPalavra("Palavra 4");
+        conjuntoPalavrasUnicas.adicionarPalavra("Palavra 2");
+        conjuntoPalavrasUnicas.exibirPalavrasUnicas();
+
+        conjuntoPalavrasUnicas.removerPalavra("Palavra 2");
+        conjuntoPalavrasUnicas.exibirPalavrasUnicas();
+
+        conjuntoPalavrasUnicas.removerPalavra("Palavra 5");
+        System.out.println(conjuntoPalavrasUnicas.verificarPalavra("Palavra 2"));
+
+        conjuntoPalavrasUnicas.exibirPalavrasUnicas();
+    }
+}
