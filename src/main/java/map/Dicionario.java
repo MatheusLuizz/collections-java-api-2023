2c2fb8ba3d25eeb6857bdfa37bac762bbc6245dd
Matheus Luiz <matheus.luizf35@gmail.com>
2023-08-21 21:07:39 -0300

    Finalizando exercicios sobre operações básicas com Map
    Signed-off-by: Matheus Luiz <matheus.luizf35@gmail.com>

---
+++ src/main/java/map/Dicionario.java
@@ -0,0 +1,53 @@
+package main.java.map;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Dicionario {
+    private Map<String, String> palavrasMap;
+
+    public Dicionario() {
+        this.palavrasMap = new HashMap<>();
+    }
+
+    public void adicionarPalavra(String palavra, String definicao) {
+        palavrasMap.put(palavra, definicao);
+    }
+
+    public void removerPalavra(String palavra) {
+        palavrasMap.remove(palavra);
+    }
+
+    public void exibirPalavras() {
+        System.out.println(palavrasMap);
+    }
+
+    public String pesquisarPorPalavra(String palavra) {
+        if (!palavrasMap.isEmpty()) {
+            return palavrasMap.get(palavra);
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Dicionario dicionario = new Dicionario();
+
+        // Adicionar palavras
+        dicionario.adicionarPalavra("maçã", "Fruta comestível de polpa suculenta e sabor doce.");
+        dicionario.adicionarPalavra("casa", "Lugar onde alguém mora.");
+
+        // Exibir todas as palavras
+        dicionario.exibirPalavras();
+
+        // Pesquisar uma palavra
+        String definicaoMaca = dicionario.pesquisarPorPalavra("maçã");
+        System.out.println("Definição da palavra 'maçã': " + definicaoMaca);
+        String definicaoUva = dicionario.pesquisarPorPalavra("uva");
+        System.out.println("Definição da palavra 'uva': " + definicaoUva);
+
+        // Remover uma palavra
+        dicionario.removerPalavra("casa");
+        System.out.println("Após remover a palavra 'casa':");
+        dicionario.exibirPalavras();
+    }
+}
